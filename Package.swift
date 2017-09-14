502342270936196575ac803de901f4cd87dcbfa2
ainame <s.namai.09@gmail.com>
2017-09-15 05:40:58 +0900

    Add Kanna

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     dependencies: [
       // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/ainame/Lumpik.git", from: "0.0.1")
+      .package(url: "https://github.com/ainame/Lumpik.git", from: "0.0.1"),
+      .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
